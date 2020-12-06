1e6361fc1ecc7b1cbcaaddf10f4a566a73b591eb
stemaDev <reg@stemadev.com>
2020-12-06 11:04:32 +0100

    hotend pid values

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -499,9 +499,9 @@
     #define DEFAULT_Ki_LIST {   1.08,   1.08 }
     #define DEFAULT_Kd_LIST { 114.00, 114.00 }
   #else
-    #define DEFAULT_Kp  22.20
-    #define DEFAULT_Ki   1.08
-    #define DEFAULT_Kd 114.00
+    #define DEFAULT_Kp  19.90
+    #define DEFAULT_Ki   1.26
+    #define DEFAULT_Kd  78.10
   #endif
 #endif // PIDTEMP
 
